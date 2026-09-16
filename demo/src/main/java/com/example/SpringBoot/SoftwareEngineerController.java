a341b26de84187c249c0362738f3b495a2d768ae
dhanush-rao-k <dhanushraokona@gmail.com>
2026-09-17 01:40:06 +0530

    Added full CRUD to postgres backend through SpringBoot API

--- demo/src/main/java/com/example/SpringBoot/SoftwareEngineerController.java
+++ demo/src/main/java/com/example/SpringBoot/SoftwareEngineerController.java
@@ -1,8 +1,13 @@
 package com.example.SpringBoot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -19,5 +24,28 @@ public class SoftwareEngineerController {
     @GetMapping
     public List<SoftwareEngineer> getSoftwareEngineers() {
         return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getSoftwareEngineers(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer)
+    {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);   
+    }
+
+    @PutMapping("{id}")
+    public void modifyOldSoftwareEngineer(@PathVariable Integer id, @RequestBody SoftwareEngineer softwareEngineer)
+    {
+        softwareEngineerService.modifySoftwareEngineer(id, softwareEngineer);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteSoftwareEngineer(@PathVariable Integer id)
+    {
+        softwareEngineerService.deleteSoftwareEngineer(id);
     }
 }
--- demo/src/main/java/com/example/SpringBoot/SoftwareEngineerService.java
+++ demo/src/main/java/com/example/SpringBoot/SoftwareEngineerService.java
@@ -15,5 +15,29 @@ public class SoftwareEngineerService {
     public List<SoftwareEngineer> getAllSoftwareEngineers(){
         return softwareEngineerRepository.findAll();
     }
+
+    public void insertSoftwareEngineer (SoftwareEngineer softwareEngineer){
+        softwareEngineerRepository.save(softwareEngineer);
+    }   
+
+    public SoftwareEngineer getSoftwareEngineerById(Integer id){
+        return softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalStateException("Id not found : " + id));   
+    }   
     
+    public void modifySoftwareEngineer(Integer id, SoftwareEngineer newSoftwareEngineer){
+        SoftwareEngineer softwareEngineerById = softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalStateException("Id not found : " + id));
+        if(newSoftwareEngineer.getName() != null){
+            softwareEngineerById.setName(newSoftwareEngineer.getName());
+        }
+        if(newSoftwareEngineer.getTechStack() != null){
+            softwareEngineerById.setTechStack(newSoftwareEngineer.getTechStack());
+        }
+        softwareEngineerRepository.save(softwareEngineerById);
+    }
+
+    public void deleteSoftwareEngineer(Integer id){
+        SoftwareEngineer softwareEngineerById = softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalStateException("Id not found : " + id));
+        softwareEngineerRepository.delete(softwareEngineerById);
+    }
+
 }
